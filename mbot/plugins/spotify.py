50dd6a56e386414f4ace869afc05ee10ba41bc62
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-11-26 17:30:58 +0530

    Update spotify.py

--- mbot/plugins/spotify.py
+++ mbot/plugins/spotify.py
@@ -563,7 +563,7 @@ async def spotify_dl(Mbot,message: Message):
         try:
             await message.reply_text(f"Done✅",   
          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Feedback", callback_data="feed")]]))
-            await message.reply_text(f"Check out @spotify_downloa (music)  @spotifynewss(News)")
+            await message.reply_text(f"Check out @SONGS_WORLDS_OFFICIAL (music)  @Resso_support(News)")
             await m.delete()
         except:
             pass 
@@ -573,7 +573,7 @@ async def spotify_dl(Mbot,message: Message):
 async def feedback(Mbot,query):
       try:
           K = await query.message.edit(f"Feedback 🏴‍☠️",
-                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Press here", url="https://t.me/dailychannelsbot?start=spotify_downloa_bot")]]))
+                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Press here", url="https://t.me/Resso_support")]]))
           H = print("New Feedback")
           if BUG:
              await copy(K,H)
@@ -586,7 +586,7 @@ async def bug(_,query):
           K = await query.message.edit(f'please report to the dev say "private version" with above  error occurred message')
           await sleep(2.3)
           H = await query.message.edit(f"Bug Report 🪲",
-                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Report to dev ", url="https://t.me/masterolic")]]))
+                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Report to dev ", url="https://t.me/insane_of_telegram")]]))
           if BUG:
              await copy(K,H)
       except Exception as e:
